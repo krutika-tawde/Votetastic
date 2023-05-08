e0c4822ba7e56f04877394b2a90783b1dd8d1578
Krutika Tawde <tawde.krutika.in@gmail.com>
2023-05-08 17:47:45 +0530

    Tailwind and Login  WIP.

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit{
+
+  form: FormGroup
+
+  constructor(private fb: FormBuilder){
+    this.form = this.fb.group({
+      email: ['tawde.krutika.in@gmail.com',[Validators.required,Validators.email]],
+      password:['123456789', [Validators.required,Validators.minLength(6)]]
+    })
+
+  }
+  ngOnInit(): void {
+  }
+
+  login(){
+    console.log(this.form.value)
+  }
+}
